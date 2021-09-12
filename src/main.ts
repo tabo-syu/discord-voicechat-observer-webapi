ad1489df63ade03ecbb0ad75d97bd70c4f83b56e
bambshot <takeshima.nitkc@gmail.com>
2021-09-13 02:57:48 +0900

    add heroku config

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.ORIGIN || 'http://localhost:3000',
   });
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
