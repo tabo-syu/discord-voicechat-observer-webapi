a246ced1d9824b82ed9b2703b9354a6f4373204b
bambshot <takeshima.nitkc@gmail.com>
2021-09-12 14:50:23 +0900

    add session-sessionlog endpoint

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { SessionResponse } from 'src/types';
+import { SessionResponse, SessionLogResponse, UserResponse } from 'src/types';
 import { SessionService } from '../service/session.service';
+import { SessionLogService } from '../service/sessionLog.service';
+import { UserService } from '../service/user.service';
 
 @Controller('sessions')
 export class SessionController {
-  constructor(private readonly sessionService: SessionService) {}
+  constructor(
+    private readonly sessionService: SessionService,
+    private readonly sessionLogService: SessionLogService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   async getSessions(): Promise<SessionResponse[]> {
@@ -14,5 +20,36 @@ export class SessionController {
   @Get(':id')
   async findSession(@Param('id') id: string): Promise<SessionResponse> {
     return await this.sessionService.session({ id });
+  }
+
+  @Get(':id/sessionLogs')
+  async findSessionLogs(
+    @Param('id') id: string,
+  ): Promise<SessionLogResponse[]> {
+    return await this.sessionLogService.sessionLogs({
+      where: {
+        sessionId: id,
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+    });
+  }
+
+  @Get(':id/users')
+  async findUsers(@Param('id') id: string): Promise<UserResponse[]> {
+    const users = await this.userService.users({
+      where: {
+        SessionLogs: {
+          some: {
+            Session: {
+              id: id,
+            },
+          },
+        },
+      },
+    });
+
+    return users;
   }
 }
